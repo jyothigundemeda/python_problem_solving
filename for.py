9ae1b40015a8d0f704a98a7c42648e6a5de4b02a
jyothigundemeda@gmail.com <jyothigundemeda@gmail.com>
2025-07-03 16:04:07 +0530

    add

--- for.py
+++ for.py
@@ -1,2 +1,37 @@
 for i in range(5):
     print(i)
+
+#break
+print("using break")
+
+for i in range(1, 10):
+    if i == 3:
+        break
+    print(i)
+
+#continue
+print("using continue")
+
+for i in range(1, 10):
+    if i == 5:
+        continue
+    print(i)
+
+#list
+print("using list")
+
+list = [1, 2, 3, 4]
+for i in list:
+    print(i)
+
+list = ["apple", "mango", "cherry"]
+for i in list:
+    print(i)
+
+list = [1,2, 3,4]
+for i in list:
+    if i%2 == 0:
+        print(i)
+
+
+
